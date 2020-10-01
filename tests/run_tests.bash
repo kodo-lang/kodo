6681a909c447a548b0796a2a11f27b2309f28e92
Owen Smith <ojman101@gmail.com>
2020-10-01 18:48:37 +0100

    tests: Add a few tests

---
+++ tests/run_tests.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+run_test() {
+    printf "Running test '%s' " $1
+    OUTPUT=$(./build-debug/compiler/compiler --silent tests/$1)
+    if [ $2 -ne $? ] || [ "$3" != "$OUTPUT" ]
+    then
+        printf "\u001b[31mFAILED\u001b[0m\n"
+        OUTPUT=${OUTPUT::-5}
+        while IFS= read -r line; do
+            printf "  %s\n" "$line"
+        done <<< "$OUTPUT"
+        printf "\u001b[0m"
+    else
+        printf "\u001b[32mOK\u001b[0m\n"
+    fi
+}
+
+run_test "implicit_extension.lang" 10 ""
+run_test "libc_hi.lang" 0 "Hi"
+run_test "malloc.lang" 0 "A"
