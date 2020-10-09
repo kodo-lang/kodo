20b30e73cee1519989c65ce153f7d51e85594079
Owen Smith <ojman101@gmail.com>
2020-10-09 23:03:38 +0100

    tests: Add some variable tests

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -23,10 +23,13 @@ run_test "hello_world.lang" 0 "Hello, world!"
 run_test "implicit_extension.lang" 10 ""
 run_test "libc_hi.lang" 0 "Hi"
 run_test "malloc.lang" 0 "A"
+run_test "mutability.lang" 20 ""
 run_test "simple_if.lang" 0 "AAA"
 
 # Expecting compile error.
 run_test "bad_if.lang" 1 "error: cannot implicitly cast from 'i32' to 'bool' on line 2
+ note: Aborting due to previous errors"
+run_test "bad_mutability.lang" 1 "error: attempted assignment of immutable variable 'foo' on line 3
  note: Aborting due to previous errors"
 run_test "type_errors.lang" 1 "error: 'test' requires 2 arguments, but 0 were passed on line 8
 error: cannot implicitly cast from 'i32' to 'i32*' on line 9
@@ -35,3 +38,9 @@ error: cannot implicitly cast from 'i32*' to 'i32' on line 9
 run_test "unknown_symbols.lang" 1 "error: no symbol named 'bar' in current context on line 2
 error: no function named 'test' in current context on line 2
  note: Aborting due to previous errors"
+run_test "use_before_init.lang" 1 "error: use of variable 'a' before initialization on line 6
+ note: Aborting due to previous errors"
+
+# Expecting compile warning.
+run_test "unused_var.lang" 0 " warn: variable 'foo' is unused on line 2
+ warn: variable 'bar' is unused on line 2"
