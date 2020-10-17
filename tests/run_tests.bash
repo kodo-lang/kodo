cad7e20a14b2016d825060617f581e9d18d40658
Owen Smith <ojman101@gmail.com>
2020-10-17 15:37:49 +0100

    tests: Separate tests into expecting compile error or success

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -16,29 +16,29 @@ run_test() {
     fi
 }
 
-# Expecting success.
-run_test "comparison.lang" 1 ""
-run_test "complex_expression.lang" 55 ""
-run_test "hello_world.lang" 0 "Hello, world!"
-run_test "implicit_extension.lang" 10 ""
-run_test "libc_hi.lang" 0 "Hi"
-run_test "malloc.lang" 0 "A"
-run_test "mutability.lang" 20 ""
-run_test "simple_if.lang" 0 "AAA"
-run_test "type_alias.lang" 5 ""
-
 # Expecting compile error.
-run_test "bad_if.lang" 1 "error: cannot implicitly cast from 'i32' to 'bool' on line 2
+run_test "compile-error/bad_if.lang" 1 "error: cannot implicitly cast from 'i32' to 'bool' on line 2
  note: Aborting due to previous errors"
-run_test "bad_mutability.lang" 1 "error: attempted assignment of immutable variable 'foo' on line 3
+run_test "compile-error/bad_mutability.lang" 1 "error: attempted assignment of immutable variable 'foo' on line 3
  note: Aborting due to previous errors"
-run_test "type_errors.lang" 1 "error: 'test' requires 2 arguments, but 0 were passed on line 9
+run_test "compile-error/type_errors.lang" 1 "error: 'test' requires 2 arguments, but 0 were passed on line 9
 error: cannot implicitly cast from 'i32' to 'i32*' on line 10
 error: cannot implicitly cast from 'i32*' to 'i32' on line 10
  note: Aborting due to previous errors"
-run_test "unknown_symbols.lang" 1 "error: no symbol named 'bar' in current context on line 2
+run_test "compile-error/unknown_symbols.lang" 1 "error: no symbol named 'bar' in current context on line 2
 error: no function named 'test' in current context on line 2
  note: Aborting due to previous errors"
-run_test "use_before_init.lang" 1 "error: use of possibly uninitialised variable 'a' on line 3
+run_test "compile-error/use_before_init.lang" 1 "error: use of possibly uninitialised variable 'a' on line 3
 error: use of possibly uninitialised variable 'c' on line 8
  note: Aborting due to previous errors"
+
+# Expecting success.
+run_test "success/comparison.lang" 1 ""
+run_test "success/complex_expression.lang" 55 ""
+run_test "success/hello_world.lang" 0 "Hello, world!"
+run_test "success/implicit_extension.lang" 10 ""
+run_test "success/libc_hi.lang" 0 "Hi"
+run_test "success/malloc.lang" 0 "A"
+run_test "success/mutability.lang" 20 ""
+run_test "success/simple_if.lang" 0 "AAA"
+run_test "success/type_alias.lang" 5 ""
