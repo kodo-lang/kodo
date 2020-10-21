35c9af8c0e1206446d48f1112969694f0cf274b1
Owen Smith <ojman101@gmail.com>
2020-10-21 21:55:11 +0100

    compiler: Abort on assertion failure

--- compiler/support/Assert.cc
+++ compiler/support/Assert.cc
@@ -5,5 +5,5 @@
 [[noreturn]] void assertion_failed(const char *file, unsigned int line, const char *expr) {
     print_error("Assertion '{}' failed at {}:{}", expr, file, line);
     print_note("This is a compiler bug!");
-    std::exit(1);
+    std::abort();
 }
