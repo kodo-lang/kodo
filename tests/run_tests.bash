5e4b10d7cb1025f9b77eeabd79fe07e769453bf2
Owen Smith <ojman101@gmail.com>
2020-10-26 13:24:14 +0000

    tests: Start using type inference

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -25,9 +25,9 @@ error: attempted assignment of immutable variable 'foo' on line 7
 run_test "compile-error/bad_pointer_mutability.lang" 1 "error: cannot implicitly cast from '*i32' to '*mut i32' on line 10
 error: attempted assignment of 'i32' value pointed to by an immutable pointer on line 2
  note: Aborting due to previous errors"
-run_test "compile-error/type_errors.lang" 1 "error: 'test' requires 2 arguments, but 0 were passed on line 8
-error: cannot implicitly cast from 'i32' to '*mut i32' on line 9
-error: cannot implicitly cast from '*i32' to 'i32' on line 9
+run_test "compile-error/type_errors.lang" 1 "error: 'test' requires 2 arguments, but 0 were passed on line 7
+error: cannot implicitly cast from 'i32' to '*mut i32' on line 8
+error: cannot implicitly cast from '*i32' to 'i32' on line 8
  note: Aborting due to previous errors"
 run_test "compile-error/unknown_symbols.lang" 1 "error: no symbol named 'bar' in current context on line 2
 error: no function named 'test' in current context on line 2
