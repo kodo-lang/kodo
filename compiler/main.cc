a06b72d7f2f1fce3962622936a0d672b5e89b518
Owen Smith <yeeetari@gmail.com>
2020-10-31 21:21:24 +0000

    compiler: Add some missing LLVM includes

--- compiler/main.cc
+++ compiler/main.cc
@@ -11,6 +11,8 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Host.h>
 #include <llvm/Support/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
